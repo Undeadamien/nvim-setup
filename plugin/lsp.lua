1f40456c2b08e68c899b4fe1704590b073fe9ae6
Undeadamien <damien.trala@outlook.com>
2024-04-24 18:18:53 +0200

    reformated the lsp file

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -3,7 +3,6 @@ require("norme").setup({
 	cmd = os.getenv("HOME") .. "/.local/bin/norminette",
 })
 vim.diagnostic.config({
-       virtual_text = true,
-       signs = false,
-	}
-	)
+	virtual_text = true,
+	signs = false,
+})
