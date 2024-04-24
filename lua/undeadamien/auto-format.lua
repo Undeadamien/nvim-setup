5fcf7b4df9dcf9673c56601c690515bbd3d4d02e
Undeadamien <damien.trala@outlook.com>
2024-04-24 18:16:23 +0200

    removed sign from lsp and correct the autoformat file

--- lua/undeadamien/auto-format.lua
+++ lua/undeadamien/auto-format.lua
@@ -1,15 +1,15 @@
 local function format_file()
 	local filetype = vim.bo.filetype
-	if filetype == "c" then
-		vim.api.nvim_command("Stdheader")
-		local command = "silent w | !c_formatter_42 < % %"
+	if filetype == "c" or filetype == "h" then
+		vim.api.nvim_command("silent w | silent !c_formatter_42 < % %")
 	end
 end
+
 local function create_header()
 	local filetype = vim.bo.filetype
-	if filetype == "c" then
+	if filetype == "c" or filetype == "h" then
 		vim.api.nvim_command("Stdheader")
 	end
 end
-vim.api.nvim_create_autocmd({ "BufAdd" }, { callback = create_header })
+vim.api.nvim_create_autocmd({ "BufEnter" }, { callback = create_header })
 vim.api.nvim_create_autocmd({ "BufWritePost" }, { callback = format_file })
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -2,3 +2,8 @@ require("null-ls").setup({ ... })
 require("norme").setup({
 	cmd = os.getenv("HOME") .. "/.local/bin/norminette",
 })
+vim.diagnostic.config({
+       virtual_text = true,
+       signs = false,
+	}
+	)
