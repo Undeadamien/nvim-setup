203357914fcdb8fa0948da690d19f114b56f3f04
Undeadamien <damien.trala@outlook.com>
2024-05-13 12:31:41 +0200

    removed the header autocommand

--- lua/undeadamien/auto-format.lua
+++ lua/undeadamien/auto-format.lua
@@ -4,12 +4,4 @@ local function format_file()
 		vim.api.nvim_command("silent w | silent !c_formatter_42 < % %")
 	end
 end
-
-local function create_header()
-	local filetype = vim.bo.filetype
-	if filetype == "c" or filetype == "h" then
-		vim.api.nvim_command("Stdheader")
-	end
-end
-vim.api.nvim_create_autocmd({ "BufEnter" }, { callback = create_header })
 vim.api.nvim_create_autocmd({ "BufWritePost" }, { callback = format_file })
